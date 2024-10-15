bbfe5963eee7ce68b2111c5abd1923c5b66662c9
Javier <jotaviervillava@gmail.com>
2024-10-15 17:31:00 +0200

    3: Prueba almacenaje BBDD

--- PruebApi.php
+++ PruebApi.php
@@ -1,11 +1,15 @@
 <?php
     require_once 'ConnectionApi.php';
     require_once 'Movie.php';
+    require_once 'BBDD.php';
 
     // Mostrar la lista de los json de las peliculas
     $connection = new ConnectionAPI('731e41f');
     $movies = $connection->searchMovieByName('Star Wars');
     echo json_encode($movies);
+    $movies_list = array();
+    $bbdd = BBDD::getInstance();
+    $bbdd_connection = $bbdd->getConnection();
     foreach($movies as $movie){
         $json = $connection->getMovieJson($movie);
         $actualMovie = Movie::fromJson($json);
@@ -14,4 +18,9 @@
         echo 'Año: ' . $actualMovie->getYear() . '<br>';
         echo 'Trama: ' . $actualMovie->getPlot() . '<br>';
         echo '<br>';
+        $query = "insert into movie (Id, Year, Plot, Title) values ('" . $bbdd_connection->real_escape_string($actualMovie->getId()) . "', " . $actualMovie->getYear() . ", '" . $bbdd_connection->real_escape_string($actualMovie->getPlot()) . "','" . $bbdd_connection->real_escape_string($actualMovie->getTitle()) . "')";
+        echo "Consulta: " . $query . "<br>";
+
+        $result = $bbdd_connection->query($query);
+        echo 'Resultado: ' .  $result . '<br>';
     }
